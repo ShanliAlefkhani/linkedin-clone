fa219a15aee85a02bdab46c1c8eaf8197ca99069
shanli <shanli.alefkhani@gmail.com>
2021-02-14 19:29:31 +0330

    Add jobs app Add Job model

---
+++ jobs/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from users.models import Company
+
+
+class Job(models.Model):
+    FIELDS_CHOICES = (
+        ('P', 'Programmer'),
+        ('T', 'Tailor'),
+        ('M', 'Mechanical Engineer'),
+        ('O', 'Other')
+    )
+    owner_company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='Job-Image/')
+    expire_date = models.DateField()
+    field = models.CharField(choices=FIELDS_CHOICES, max_length=1)
+    salary = models.IntegerField(default=0)
+    hours = models.IntegerField()
