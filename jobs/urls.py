e62f479a2e831786b0d59f7d79f091df4625c0c5
shanli <shanli.alefkhani@gmail.com>
2021-02-14 21:12:25 +0330

    Add Apply view to urls

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from jobs.views import JobList, JobCreate, JobUpdate
+from jobs.views import JobList, JobCreate, JobUpdate, Apply
 
 urlpatterns = [
     path('job-list/', JobList.as_view()),
     path('job-create/', JobCreate.as_view()),
     path('job-update/<pk>/', JobUpdate.as_view()),
+    path('apply/', Apply.as_view()),
 ]
