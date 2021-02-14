f870d1fad6e7816d67f897eafb8e12c4fa59eaef
shanli <shanli.alefkhani@gmail.com>
2021-02-14 17:37:53 +0330

    Add PersonList and CompanyList to views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from users.models import Person, Company
+from users.serializers import PersonSerializer, CompanySerializer
 
-# Create your views here.
+
+class PersonList(generics.ListCreateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+
+class CompanyList(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
