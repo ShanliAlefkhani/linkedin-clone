7eb1503312c0901f4fb0037b5f95a69dc696a6cd
shanli <shanli.alefkhani@gmail.com>
2021-02-14 18:18:55 +0330

    Add SignUpPerson view and SignUpCompany view to urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from users import views
+from users.views import SignUpPerson, SignUpCompany
 
 urlpatterns = [
-    path('person-list/', views.PersonList.as_view()),
-    path('company-list/', views.CompanyList.as_view()),
+    path('list-person/', views.PersonList.as_view()),
+    path('list-company/', views.CompanyList.as_view()),
+    path('signup-person/', SignUpPerson.as_view()),
+    path('signup-company/', SignUpCompany.as_view()),
 ]
--- yektanet_interview_project/settings.py
+++ yektanet_interview_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
