6e1da7d1969a50b213c0e33f98bdfd3e525b884e
shanli <shanli.alefkhani@gmail.com>
2021-02-14 18:04:34 +0330

    Add SignUpPerson and SignUpCompany to views

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
-from users.models import Person, Company
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from users.models import Person, Company, User
 from users.serializers import PersonSerializer, CompanySerializer
 
 
@@ -11,3 +13,29 @@ class PersonList(generics.ListCreateAPIView):
 class CompanyList(generics.ListCreateAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+
+class SignUpPerson(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    queryset = User.objects.all()
+    serializer_class = PersonSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = PersonSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=ValueError):
+            serializer.create(validated_data=request.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SignUpCompany(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    queryset = User.objects.all()
+    serializer_class = CompanySerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid(raise_exception=ValueError):
+            serializer.create(validated_data=request.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
