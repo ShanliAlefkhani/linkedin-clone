a9bf7ba1021db21bda0544f812d889c345e4b421
shanli <shanli.alefkhani@gmail.com>
2021-02-14 22:55:49 +0330

    Add ApplicationList view to urls

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from jobs.views import JobList, JobCreate, JobUpdate, Apply
+from jobs.views import JobList, JobCreate, JobUpdate, Apply, ApplicationList
 
 urlpatterns = [
     path('job-list/', JobList.as_view()),
     path('job-create/', JobCreate.as_view()),
     path('job-update/<pk>/', JobUpdate.as_view()),
     path('apply/', Apply.as_view()),
+    path('job-applications/<pk>/', ApplicationList.as_view()),
 ]
